214be3fe0f936f692c7c292e41af3e89389e24fb
beomboo <BeomBoo@gsitm.com>
2024-01-09 16:32:03 +0900

    init

---
+++ mobileutilsUITests/mobileutilsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mobileutilsUITestsLaunchTests.swift
+//  mobileutilsUITests
+//
+//  Created by GSITM on 1/9/24.
+//
+
+import XCTest
+
+final class mobileutilsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
