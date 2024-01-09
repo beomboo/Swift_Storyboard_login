f5f5a3fa34c3619a8d22fc1b79fc0c9c3d4b60e5
beomboo <BeomBoo@gsitm.com>
2024-01-09 18:12:48 +0900

    기본 구조 설정

---
+++ mobileutils/logger/Logger.swift
@@ -0,0 +1,8 @@
+//
+//  Logger.swift
+//  mobileutils
+//
+//  Created by GSITM on 1/9/24.
+//
+
+import Foundation
---
+++ mobileutils/logger/MyFileManager.swift
@@ -0,0 +1,12 @@
+//
+//  FileManager.swift
+//  mobileutils
+//
+//  Created by GSITM on 1/9/24.
+//
+
+import Foundation
+
+class myFileManager {
+    
+}
